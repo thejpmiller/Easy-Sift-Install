bd68649d8a28c065fbb413a684c7a711499588b7
Joshua Miller <joshua@thejpmiller.com>
2023-01-31 01:52:51 -0800

    Added a few changes. Included echo comments in case parts of the application broke.

--- EasySiftInstall.sh
+++ EasySiftInstall.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Improvement Options:
+# Write a function/part to ensure user is downloading the latest version.
 
 # SIFT github : https://github.com/teamdfir/sift-cli/
 # Referenced : https://www.youtube.com/watch?v=yXuSPZEUcYk
@@ -19,11 +21,8 @@ if grep -q "$UBUNTUVALUE" /etc/lsb-release; then
     echo "WSL is in the correct Ubuntu version."
 fi
 
-CheckingUbuntuVersion();
-
 echo "Checking if SIFT prerequists are meet?"
-
-echo "Downloading sift prerequisists."
+echo "Downloading SIFT prerequisists."
 
 # Downloading #
 # sift-cli-linux
@@ -38,12 +37,18 @@ wget -v "https://github.com/teamdfir/sift-cli/releases/download/v1.14.0-rc1/sift
 # download go
 wget "https://dl.google.com/go/go1.15.4.linux-amd64.tar.gz" -P $DOWNLOADLOCATION
 
-sudo tar -xvf $HOME/Downloads/go1.16linux-amd64.tar.gz
+# extracting go version
+sudo tar -xvf $HOME/Downloads/go1.15.4.linux-amd64.tar.gz
 
+# installing in /usr/local directory
 sudo mv go "/usr/local"
+
+# assigning a few GO Lang specific variables
 export GOROOT=/usr/local/go
 export GOPATH=/var/tmp
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# what version is GO
 go version
 
 wget "https://github.com/sigstore/cosign/releases/download/v1.13.1/cosign-linux-amd64" -P $DOWNLOADLOCATION
@@ -51,18 +56,26 @@ wget "https://github.com/sigstore/cosign/releases/download/v1.13.1/cosign-linux-
 #make it executable
 sudo mv cosign-linux-amd64 /usr/local/bin/cosign
 
-chmod +x /usr/local/bin/cosign
+# set file permission
+sudo chmod +x /usr/local/bin/cosign
 
+# install go language
 go install
 
+echo "Verifying file signatures"
 cosign verify-blob --key sift-cli.pub --signature sift-cli-linux.sig sift-cli-linux
 
+# moving sift-cli-linux files to /usr/local/bin/sift, installing it.
 sudo mv sift-cli-linux /usr/local/bin/sift
 
+# set file permissions
 chmod 755 /usr/local/bin/sift
 
 # testing if it is install...
 sift --help
 
+echo "Installing SIFT"
 # execute this command, it must be this specifically for WSL installation
 sudo sift install --mode=server
+
+echo "Finished. Thank you for using EASY SIFT INSTALL for WSL."
